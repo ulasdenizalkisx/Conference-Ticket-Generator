c4b084bd16fcf261111c09452cd4fe1e6a83041f
Ulaş Deniz Alkış <24100001033@stu.yasar.edu.tr>
2025-07-13 03:54:23 +0300

    v4

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var file_empty = true;
 document.getElementById('file_upload').addEventListener('change', function () {
     const file = this.files[0];
     const Size = 500 * 1024;
@@ -8,9 +9,11 @@ document.getElementById('file_upload').addEventListener('change', function () {
             span.className = 'file_info_text_error';
             span.innerText = 'File too large. Please upload a photo under 500KB.';
             img.src = 'assets/images/info_icon_error.png';
+            file_empty = true;
             this.value = '';
         } 
         else {
+            file_empty = false;
             span.className = 'file_info_text';
             span.innerText = 'Upload your photo (JPG or PNG, max size: 500KB).';
             img.src = 'assets/images/icon-info.svg';
@@ -19,7 +22,6 @@ document.getElementById('file_upload').addEventListener('change', function () {
             photo.style.padding = '0';
             photo.src = tempURL;
 
-            // Remove & Change buttons (with working remove)
             document.getElementById('fileUploadText').innerHTML = '<div id="alignerdiv" class="alignerDiv"><button class="rem_chg_buttons1">Remove image</button><button id="img_change_button" class="rem_chg_buttons">Change image</button></div>';
         }
     }
@@ -35,14 +37,25 @@ document.addEventListener('click', function(event) {
 });
 
 function ticketGenerator() {
-    const fname = document.getElementById('full_name').value;
-    const text = `<span> Congrats, <span class="gradient">${fname}</span><br>Your ticket is ready.</span>`;
-    document.getElementById('header_h1').innerHTML = text;
-
-    const email = document.getElementById('email_address').value;
-    const text2 = `<span>We've emailed your ticket to <br><span class="orange_email">${email}</span> and will send updates in <br>the run up to the event.</span>`;
-    document.getElementById('header_h2').innerHTML = text2;
-
-    document.getElementById('container').style.display = "none";
-
+        if (file_empty == false) {
+        const email = document.getElementById('email_address').value;
+        if (email != '' && email.includes('@') && email.includes('.com')) {
+            const icon = document.getElementById('email_error_icon');
+            icon.src = '';
+            document.getElementById('email_error_text').innerText = '';
+            document.getElementById('error_email').style.display = 'none';
+            const text2 = `<span>We've emailed your ticket to <br><span class="orange_email">${email}</span> and will send updates in <br>the run up to the event.</span>`;
+            document.getElementById('header_h2').innerHTML = text2;
+        }
+        else {
+            const icon = document.getElementById('email_error_icon');
+            icon.src = 'assets/images/info_icon_error.png';
+            document.getElementById('email_error_text').innerText = 'Please enter a valid email address.';
+            return false;
+        }
+        const fname = document.getElementById('full_name').value;
+        const text = `<span> Congrats, <span class="gradient">${fname}</span><br>Your ticket is ready.</span>`;
+        document.getElementById('header_h1').innerHTML = text;
+        document.getElementById('container').style.display = "none";
+    }
 }
